b06e4ade8adac75564e77a70437968d3437f96be
allanMoyaM <allanmm29@gmail.com>
2016-05-10 15:50:57 -0600

    Cambios de eleccion

--- Project1_SmartTourCR/resources/views/principal.blade.php
+++ Project1_SmartTourCR/resources/views/principal.blade.php
@@ -89,52 +89,109 @@
         </li>
       </ul>
     </div>
-  </div>
-</nav>
+  </nav>
 
-<div class="content">
-  <div class="container-fluid">
-    <div class="row">
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
 
-      <!-- Insertar aquí lo visible al usuario-->
+        <!-- Insertar aquí lo visible al usuario-->
+        <div class="col-lg-4">
+          <form role="form">
 
-    </div>
-  </div>
-</div>
-    <div class="footer">
-      <div class="container-fluid">
-        <nav class="pull-left">
-          <ul>
-            <li>
-              <a href="#">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                Company
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                Portfolio
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                Blog
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <p class="copyright pull-right">
-          &copy; 2016 <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
-        </p>
+            <div class="form-group">
+              <label>Lugar</label>
+              <select name="lugar" id="lugar" class="form-control" tabindex="9">
+                <option value="0">San José</option>
+                <option value="1">Alajuela</option>
+                <option value="2">Cartago</option>
+                <option value="3">Guanacaste</option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <label>Precio</label>
+              <select name="lugar" id="lugar" class="form-control" tabindex="9">
+                <option value="0">Gratis</option>
+                <option value="1">1000-5000 colones</option>
+                <option value="2">5000-10000 colones</option>
+                <option value="3">10000-15000 colones</option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <label>Tipo de camino</label>
+              <select name="lugar" id="lugar" class="form-control" tabindex="9">
+                <option value="0">Asfaltado</option>
+                <option value="1">Pedregoso</option>
+                <option value="2">Lastre</option>
+                <option value="3">Barro</option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <label>Duracion</label>
+              <select name="lugar" id="lugar" class="form-control" tabindex="9">
+                <option value="0">Menos de 1 hora</option>
+                <option value="1">1-2 horas</option>
+                <option value="2">2-3 horas</option>
+                <option value="3">3-4 horas</option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <label>Clima</label>
+              <select name="lugar" id="lugar" class="form-control" tabindex="9">
+                <option value="0">Frío</option>
+                <option value="1">Lluvioso</option>
+                <option value="2">Soleado</option>
+                <option value="3">Caliente</option>
+              </select>
+            </div>
+
+          </form>
+        </div>
+
       </div>
     </div>
   </div>
+
+</div>
+
+<footer class="footer">
+  <div class="container-fluid">
+    <nav class="pull-left">
+      <ul>
+        <li>
+          <a href="#">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Company
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Portfolio
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Blog
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <p class="copyright pull-right">
+      &copy; 2016 <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
+    </p>
+  </div>
+</footer>
 </div>
 </div>
+
 </body>
 
 <!--   Core JS Files   -->
